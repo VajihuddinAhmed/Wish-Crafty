0ce85ae392a422038f6ef2ab512103c646102673
VajihuddinAhmed <vajihahmed@gmail.com>
2021-03-11 00:59:56 +0100

    Added custom button, directory, form-input components and also added checkout page

---
+++ src/pages/checkout/checkout.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import CheckoutItem from '../../components/checkout-item/checkout-item';
+import './checkout.scss';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
+import { CartContext } from '../../provider/cart/cart.provider';
+
+const CheckoutPage = () => {
+    const { cartItems, cartTotal } = useContext(CartContext)
+
+    return (
+        <div className="checkout-page">
+    
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
+    
+            {cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem} /> )}
+    
+            <div className="total">
+                <span>Total : €{cartTotal}</span>
+            </div>
+    
+            <div className="test-warning">
+                *Please use the following test credit card for payments*
+                <br/>
+                4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+            </div>
+    
+            <StripeCheckoutButton price={cartTotal} />
+    
+        </div>
+    )
+};
+
+export default CheckoutPage;
