8442d6c1bb8b9f9ee367d8fe79c00248a05bea19
VajihuddinAhmed <vajihahmed@gmail.com>
2021-03-16 00:54:57 +0100

    Added cart.provider.js and cart.utils.js in provider component for using context api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import CheckoutPage from './pages/checkout/checkout';
+import Header from './components/header/header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import CurrentUserContext from './contexts/current-user/current-user.context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() { 
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if(userAuth) {
+          const userRef = await createUserProfileDocument(userAuth)
+
+          userRef.onSnapshot(snapshot => {
+            this.setState({ currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }})
+          }) 
+      }
+
+      this.setState({ currentUser: userAuth })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <CurrentUserContext.Provider value={this.state.currentUser}>
+          <Header />
+        </CurrentUserContext.Provider>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route exact path="/signin" render={() => this.state.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage />) } />
+        </Switch>
+      </div>
+    );
+  }
+};
 
 export default App;
